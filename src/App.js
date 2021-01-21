7307906496d483c56b7ee9c98f5fa32d56b48067
dong uk <gusrn1255@naver.com>
2021-01-21 10:16:07 +0900

    논리 연산자 적용

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+  //1. 데이터 담을공간
+  const [movies, setMovies] = useState({})
+  const [loading, setLoading] = useState(true)
+
+  //3, 네트워킹
+
+  const getData = async () => {
+    return (
+        await axios.get('https://api.themoviedb.org/3/trending/all/day?api_key=8597e491ed6e80f0de12e349eb60ea6e')
+            // .then(res => console.log(res.data.results))
+            .then(res =>{
+              setMovies(res.data.results)
+              setLoading(false)
+            })
+            .catch(err => console.log(err))
+    )
+  }
+
+
+
+  //2, 자동실행함수
+  useEffect(() => {
+    getData()
+  }, {})
+
+    //4. 화면에 보여지는부분
+    return (
+        <>
+            {loading ?
+                <div>
+                    <h1>
+                        loading ...
+                    </h1>
+                </div>
+            : (
+                    <div>
+                        {movies.map(movie =>
+                            <>
+                                <h1>Movie Title : {movie.name}</h1>
+                                <h2>first Release Day : {movie.first_air_date || movie.release_date}</h2>
+                                <h3>{movie.vote_average} : 10</h3>
+                            </>
+                        )}
+                    </div>
+                )
+            }
+        </>
+
+
+    );
+};
 
 export default App;
